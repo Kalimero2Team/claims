4982e68151cfc1fd73d762b38c810bb6f9e90088
byquanton <32410361+byquanton@users.noreply.github.com>
2024-02-04 17:36:45 +0100

    Fix: Use WorldLoadEvent to get World in Squaremap Integration

--- claims-squaremap-integration/src/main/java/com/kalimero2/team/claims/squaremap/hook/SquaremapHook.java
+++ claims-squaremap-integration/src/main/java/com/kalimero2/team/claims/squaremap/hook/SquaremapHook.java
@@ -1,34 +1,47 @@
 package com.kalimero2.team.claims.squaremap.hook;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.kalimero2.team.claims.squaremap.SquaremapClaims;
 import com.kalimero2.team.claims.squaremap.task.SquaremapTask;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.WorldLoadEvent;
+import xyz.jpenilla.squaremap.api.BukkitAdapter;
 import xyz.jpenilla.squaremap.api.Key;
 import xyz.jpenilla.squaremap.api.MapWorld;
 import xyz.jpenilla.squaremap.api.SimpleLayerProvider;
 import xyz.jpenilla.squaremap.api.SquaremapProvider;
 import xyz.jpenilla.squaremap.api.WorldIdentifier;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static xyz.jpenilla.squaremap.api.Key.key;
 
-public final class SquaremapHook {
+public final class SquaremapHook implements Listener {
     private static final Key CLAIM_CHUNK_LAYER_KEY = key("claims");
 
     private final Map<WorldIdentifier, SquaremapTask> tasks = new HashMap<>();
 
     public SquaremapHook(final SquaremapClaims plugin) {
-        for (final MapWorld world : SquaremapProvider.get().mapWorlds()) {
-            final SimpleLayerProvider provider = SimpleLayerProvider.builder(plugin.config().controlLabel)
-                .showControls(plugin.config().controlShow)
-                .defaultHidden(plugin.config().controlHide)
-                .build();
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onWorldLoad(WorldLoadEvent event) {
+        final MapWorld world = SquaremapProvider.get().mapWorlds().stream()
+                .filter(mapWorld -> BukkitAdapter.bukkitWorld(mapWorld).getUID().equals(event.getWorld().getUID()))
+                .findFirst()
+                .orElse(null);
+        if (world != null) {
+            final SimpleLayerProvider provider = SimpleLayerProvider.builder(SquaremapClaims.getPlugin(SquaremapClaims.class).config().controlLabel)
+                    .showControls(SquaremapClaims.getPlugin(SquaremapClaims.class).config().controlShow)
+                    .defaultHidden(SquaremapClaims.getPlugin(SquaremapClaims.class).config().controlHide)
+                    .build();
 
             world.layerRegistry().register(CLAIM_CHUNK_LAYER_KEY, provider);
 
-            final SquaremapTask task = new SquaremapTask(plugin, world, provider);
-            task.runTaskTimerAsynchronously(plugin, 20L, 20L * plugin.config().updateInterval);
+            final SquaremapTask task = new SquaremapTask(SquaremapClaims.getPlugin(SquaremapClaims.class), world, provider);
+            task.runTaskTimerAsynchronously(SquaremapClaims.getPlugin(SquaremapClaims.class), 20L, 20L * SquaremapClaims.getPlugin(SquaremapClaims.class).config().updateInterval);
 
             this.tasks.put(world.identifier(), task);
         }
